cabbb484890132a38aca06ae3cf0386e84b470ee
Jaren Ramirez <jarenramirez2019@gmail.com>
2023-04-18 00:47:08 -0800

    Added robot needs changes after hardware is implemented

---
+++ include/Robot.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "AutoDrive.h"
+#include "UserDrive.h"
+#include "Telemetry.h"
+#include "Hardware.h"
+#include "vex.h"
+
+
+/*
+STANDARD UNITS:
+    Encoders in Rotations
+    Distance in Inches
+    Heading/Angle in Degrees
+*/
+
+
+class Robot{
+public:
+    Robot();
+    void printDistToGoal();
+    void drive();
+    void driveAuto();
+private:
+    UserDrive* userDrive;
+    AutoDrive* autoDrive;
+    Hardware* hw;
+    Telemetry* tm;
+    const int DRIVETRAINWHEELCIRCUMFERENCE = 12.57; //inches
+};
---
+++ src/Robot.cpp
@@ -0,0 +1,31 @@
+#include "Robot.h"
+
+Robot::Robot()
+{
+    hw = new Hardware();
+    tm = new Telemetry(hw, DRIVETRAINWHEELCIRCUMFERENCE);
+    userDrive = new UserDrive(hw, tm); 
+    autoDrive = new AutoDrive(hw, tm, DRIVETRAINWHEELCIRCUMFERENCE);
+}
+
+
+void Robot::drive() {
+    userDrive->drive();
+}
+
+
+void Robot::driveAuto() {
+    autoDrive->drive();
+}
+
+
+void Robot::printDistToGoal(){
+    while(1){
+        vex::wait(500,vex::msec);
+        hw->master.clear();
+        vex::wait(100,vex::msec)
+        //hw->master.print(0, 0, "Distance is %.0f", userDrive->outputDistanceToGoal());
+        hw->master.print(0, 0, "x: %.3f y: %.3f", tm->getGPSPosition().first, tm->getGPSPosition().second);
+        vex::wait(70,vex::msec)
+    }
+}
