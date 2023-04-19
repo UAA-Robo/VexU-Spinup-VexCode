647b87a17b4a3455f46da0363fa9234e79bf3975
Jaren Ramirez <jarenramirez2019@gmail.com>
2023-04-18 23:30:39 -0800

    added stopping for the drive motors

--- src/Hardware.cpp
+++ src/Hardware.cpp
@@ -1,5 +1,14 @@
 #include "Hardware.h"
 
 Hardware::Hardware() {
+     wheelLeftFront.setBrake(vex::brakeType::brake);
+     wheelLeftBack.setBrake(vex::brakeType::brake);
+     wheelRightFront.setBrake(vex::brakeType::brake);
+     wheelRightBack.setBrake(vex::brakeType::brake);
 
+    leftWheels.setStopping(vex::brakeType::brake);
+    rightWheels.setStopping(vex::brakeType::brake);
+
+    driveTrain.setStopping(vex::brakeType::brake);
+    smartDriveTrain.setStopping(vex::brakeType::brake);
 }
